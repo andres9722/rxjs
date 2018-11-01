81da1f0b7f434da2101e3c1400bf8ef08502451f
Andrés Felipe Álvarez Caballero <andres_9722@outlook.com>
2018-11-01 17:28:43 -0500

    rxjs
    Exercises

--- src/index.js
+++ src/index.js
@@ -44,3 +44,82 @@
 // setTimeout(() => {
 //   const reallyHot = hotObs.subscribe(x => addItem(`${x} Observer Four`));
 // }, 4000);
+
+// import { Subject } from "rxjs";
+
+// const subject = new Subject();
+
+// subject.subscribe(
+//   x => addItem("Obs 1 " + x),
+//   e => addItem("Obs 1 " + e),
+//   () => addItem("Obs 1 Complete")
+// );
+
+// subject.next("First thing that I emited");
+
+// const observerSub = subject.subscribe(x => addItem("Obs 2 " + x));
+
+// subject.next("Second thing that I emited");
+// observerSub.unsubscribe();
+// subject.next("Third thing that I emited");
+
+// function addItem(x) {
+//   const node = document.createElement("li");
+//   const text = document.createTextNode(x);
+//   node.appendChild(text);
+//   const ul = document.getElementById("output");
+//   ul.appendChild(node);
+// }
+
+// import { BehaviorSubject } from "rxjs";
+
+// const subject = new BehaviorSubject("First");
+
+// subject.subscribe(
+//   x => addItem("Obs 1 " + x),
+//   e => addItem("Obs 1 " + e),
+//   () => addItem("Obs 1 Complete")
+// );
+
+// subject.next("First thing that I emited");
+// subject.next("... Observer 2 is going to subscribe ...");
+// const observerSub = subject.subscribe(x => addItem("Obs 2 " + x));
+
+// subject.next("Second thing that I emited");
+// observerSub.unsubscribe();
+// subject.next("Third thing that I emited");
+
+// function addItem(x) {
+//   const node = document.createElement("li");
+//   const text = document.createTextNode(x);
+//   node.appendChild(text);
+//   const ul = document.getElementById("output");
+//   ul.appendChild(node);
+// }
+
+import { ReplaySubject } from "rxjs";
+
+const subject = new ReplaySubject(2);
+
+subject.subscribe(
+  x => addItem("Obs 1 " + x),
+  e => addItem("Obs 1 " + e),
+  () => addItem("Obs 1 Complete")
+);
+
+subject.next("First thing that I emited");
+subject.next("Another thing that I emited");
+subject.next("... Observer 2 is going to subscribe ...");
+const observerSub = subject.subscribe(x => addItem("Obs 2 " + x));
+
+subject.next("Second thing that I emited");
+observerSub.unsubscribe();
+subject.next("Third thing that I emited");
+
+function addItem(x) {
+  const node = document.createElement("li");
+  const text = document.createTextNode(x);
+  node.appendChild(text);
+  const ul = document.getElementById("output");
+  ul.appendChild(node);
+}
