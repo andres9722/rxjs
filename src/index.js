1b5410dc72ca3475dcd69a91dfe087422e945630
Andrés Felipe Álvarez Caballero <andres_9722@outlook.com>
2018-11-01 17:35:50 -0500

    rxjs
    Examples

--- src/index.js
+++ src/index.js
@@ -97,24 +97,73 @@
 //   ul.appendChild(node);
 // }
 
-import { ReplaySubject } from "rxjs";
+// import { ReplaySubject } from "rxjs";
 
-const subject = new ReplaySubject(2);
+// const subject = new ReplaySubject(3);
 
-subject.subscribe(
-  x => addItem("Obs 1 " + x),
-  e => addItem("Obs 1 " + e),
-  () => addItem("Obs 1 Complete")
-);
+// subject.subscribe(
+//   x => addItem("Obs 1 " + x),
+//   e => addItem("Obs 1 " + e),
+//   () => addItem("Obs 1 Complete")
+// );
 
-subject.next("First thing that I emited");
-subject.next("Another thing that I emited");
-subject.next("... Observer 2 is going to subscribe ...");
-const observerSub = subject.subscribe(x => addItem("Obs 2 " + x));
+// subject.next("First thing that I emited");
+// subject.next("Another thing that I emited");
+// subject.next("... Observer 2 is going to subscribe ...");
+// const observerSub = subject.subscribe(x => addItem("Obs 2 " + x));
 
-subject.next("Second thing that I emited");
-observerSub.unsubscribe();
-subject.next("Third thing that I emited");
+// subject.next("Second thing that I emited");
+// observerSub.unsubscribe();
+// subject.next("Third thing that I emited");
+
+// function addItem(x) {
+//   const node = document.createElement("li");
+//   const text = document.createTextNode(x);
+//   node.appendChild(text);
+//   const ul = document.getElementById("output");
+//   ul.appendChild(node);
+// }
+
+// import { ReplaySubject } from "rxjs";
+
+// const subject = new ReplaySubject(30, 200);
+
+// function addItem(x) {
+//   const node = document.createElement("li");
+//   const text = document.createTextNode(x);
+//   node.appendChild(text);
+//   const ul = document.getElementById("output");
+//   ul.appendChild(node);
+// }
+
+// subject.subscribe(
+//   x => addItem("Obs 1 " + x),
+//   e => addItem("Obs 1 " + e),
+//   () => addItem("Obs 1 Complete")
+// );
+
+// let i = 1;
+// let int = setInterval(() => subject.next(i++), 100);
+
+// setTimeout(() => {
+//   const obsTwo = subject.subscribe(x => addItem("Obs 2 " + x));
+// }, 501);
+
+import { AsyncSubject } from "rxjs";
+
+/** Subjects */
+
+const subject = new AsyncSubject();
+
+subject.subscribe(x => addItem("Obs 1 " + x), () => addItem("Obs 1 Complete"));
+
+let i = 1;
+let int = setInterval(() => subject.next(i++), 100);
+
+setTimeout(() => {
+  const obsTwo = subject.subscribe(x => addItem("Obs 2 " + x));
+  subject.complete();
+}, 510);
 
 function addItem(x) {
   const node = document.createElement("li");
